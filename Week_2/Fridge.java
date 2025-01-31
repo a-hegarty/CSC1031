506f7b5ec5a189742d528c65b6ef8dd003b510d4
A.Hegarty <a.hegarty004@gmail.com>
2025-01-31 21:05:53 +0000

    added Fridge.java

---
+++ Week_2/Fridge.java
@@ -0,0 +1,27 @@
+//Program name: Fridge.java
+//date: 31/01/2025
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fridge {
+    ...
+
+    public Fridge(int initialBalance) {
+       ...
+    }
+
+    public void addFood(String item, int cost) {
+        ...
+    }
+
+    public void getFood(String item) {
+       ...
+    }
+
+    public void checkStatus() {
+        ...
+    }
+
+
+}
