d5c62eeff1a1b4cae65c37cbd052023ad95ba321
A.Hegarty <a.hegarty004@gmail.com>
2025-02-27 14:21:27 +0000

    added files

--- Week_5/Vehicles.java
+++ Week_5/Vehicles.java
@@ -1,5 +1,36 @@
 package Week_5;
 
 public class Vehicles {
-    
+    interface Vehicle {
+        void start();
+        void stop();
+    }
+
+    class Car implements Vehicle {
+        //override method start()
+        @Override
+        public void start() {
+            System.out.println("Car is staring . . .");
+        }
+        
+        //override method stop
+        @Override
+        public void stop(){
+            System.out.println("Car is stopping . . .");
+        }
+    }
+
+    class Bicycle implements Vehicle {
+        //override method start()
+        @Override
+        public void start() {
+            System.out.println("Bicycle is staring . . .");
+        }
+        
+        //override method stop
+        @Override
+        public void stop(){
+            System.out.println("Bicycle is stopping . . .");
+        }
+    }
 }
